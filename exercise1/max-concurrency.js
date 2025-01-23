c7f7d808a1ecc1370267737096e16a50f31f6641
Marc <msoler955@gmail.com>
2025-01-23 13:17:15 +0100

    Comments

--- exercise1/max-concurrency.js
+++ exercise1/max-concurrency.js
@@ -1,11 +1,12 @@
 async function fetchURLsWithConcurrency(urls, maxConcurrency) {
-    const calls = [];
+    const calls = []; // I'll store all promises here in order to wait for all to finish with Promise.all
+    const currentConcurrency = 0; // I'll count how many fetch requests there are in parallel in order to block more fetch requests form executing
     async function createCall(url) {
         const fetchResponse =  await fetch(url)
         return await fetchResponse.json()
     }
     for (let i = 0; i < urls.length; ++i) {
-        calls.push( createCall(urls[i]))
+        calls.push(createCall(urls[i]))
     }
 
     return await Promise.all(calls)
